5a5bd451c0efc5cc06841f111629cf3fd68e722b
Switha <rexchan06@gmail.com>
2025-07-24 11:32:25 +0800

    feat: added hovering feature to game

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,10 +1,17 @@
+import { hover } from '@testing-library/user-event/dist/hover';
+import { useState } from 'react';
+
 function Board(prop) {
+    const [hoveredCell, setHoveredCell] = useState(null);
     return (
         <div className="bg-white p-4 rounded-3 shadow-lg border" style={{backgroundColor: '#f8f9fa'}}>
             {prop.board.map((row, row_index) =>
             <div key={row_index} className="d-flex">
-                {row.map((col, col_index) =>
-                <button 
+                {row.map((col, col_index) => {
+                const isThisCellHovered = hoveredCell && hoveredCell.row === row_index && hoveredCell.col === col_index;
+                const gameIsActive = !prop.gameEnd && !prop.gameTie;
+                const shouldShowPreview = col === '' && isThisCellHovered && gameIsActive;
+                return <button 
                     className="btn btn-light border-dark rounded-0"
                     style={{
                     width: '90px', 
@@ -15,10 +22,15 @@ function Board(prop) {
                     }}
                     key={col_index} 
                     disabled={prop.gameEnd || prop.gameTie || col !== ''}
-                    onClick={() => prop.updateBoard(row_index, col_index, prop.turn)}
+                    onMouseEnter={() => setHoveredCell({row: row_index, col: col_index})}
+                    onMouseLeave={() => setHoveredCell(null)}
+                    onClick={() => prop.updateBoard(row_index, col_index, prop.turn)
+                    }
                 >
-                    {col}
+                    
+                    {col !== '' ? col : shouldShowPreview ? (prop.turn == 0 ? 'O' : 'X') : ''}
                 </button>
+                }
                 )}
             </div>
             )}
